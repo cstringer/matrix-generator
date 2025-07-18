61b2acf7446490b4552df6c0aad1a1eb8be704dc
Chris Stringer <chris.stringer@telescript.com>
2025-07-18 16:13:46 -0500

    Formatting updates

--- matrix.js
+++ matrix.js
@@ -4,47 +4,47 @@ const NUM_ROWS = 50;
 const matrix = document.getElementById('the_matrix');
 
 function generateRandomChar(max = 0) {
-    let text = '';
-    for (let i = max; i >= 0; i--) {
-        const opacity = (max - i) / max;
-        const colorCls = i === 0 ? 'last' : '';
-        text += `<span class="char ${colorCls}" style="opacity:${opacity}">`;
-        text += String.fromCharCode(Math.floor(randomCount(92) + 65));
-        text += '</span>';
-        text += '<br />';
-    }
-    return text;
+  let text = '';
+  for (let i = max; i >= 0; i--) {
+    const opacity = (max - i) / max;
+    const colorCls = i === 0 ? 'last' : '';
+    text += `<span class="char ${colorCls}" style="opacity:${opacity}">`;
+    text += String.fromCharCode(Math.floor(randomCount(92) + 65));
+    text += '</span>';
+    text += '<br />';
+  }
+  return text;
 }
 
 function randomCount(scale) {
-    return Math.floor(Math.random() * scale);
+  return Math.floor(Math.random() * scale);
 }
 
 function makeColumn(colNum) {
-    const column = document.createElement('div');
-    column.className = 'column';
-    matrix.append(column);
+  const column = document.createElement('div');
+  column.className = 'column';
+  matrix.append(column);
 
-    let count = randomCount(NUM_ROWS);
-    let randomInterval = () => randomCount(133) + 17;
-    const startAnimation = () => setInterval(animateColumn, randomInterval());
-    let id = null;
-    const animateColumn = () => {
-        if (id % 4 !== 0) {
-            column.innerHTML = generateRandomChar(count);
-        } else {
-            column.innerHTML = '&nbsp;';
-        }
-        count++;
-        if (count >= NUM_ROWS) {
-            count = randomCount(NUM_ROWS);
-            clearInterval(id);
-            id = startAnimation();
-        }
-    };
-    id = startAnimation();
+  let count = randomCount(NUM_ROWS);
+  let randomInterval = () => randomCount(133) + 17;
+  const startAnimation = () => setInterval(animateColumn, randomInterval());
+  let id = null;
+  const animateColumn = () => {
+    if (id % 4 !== 0) {
+      column.innerHTML = generateRandomChar(count);
+    } else {
+      column.innerHTML = '&nbsp;';
+    }
+    count++;
+    if (count >= NUM_ROWS) {
+      count = randomCount(NUM_ROWS);
+      clearInterval(id);
+      id = startAnimation();
+    }
+  };
+  id = startAnimation();
 }
 
 for (let c = 0; c < NUM_COLS; c++) {
-    makeColumn(c);
+  makeColumn(c);
 }
